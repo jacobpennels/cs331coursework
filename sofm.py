80b9033dbad69f90b852d1774673705641a37d00
Jacob Pennels <jacob_pennels@hotmail.com>
2017-12-14 11:32:28 +0000

    Program complete, experimenting can begin

--- sofm.py
+++ sofm.py
@@ -33,8 +33,8 @@ v_points = []
 convergent_points = []
 temp_convergence = [0 for i in range(square)]
 convergence_values = [[] for x in range(square)]
-print(str(convergence_values))
-
+#print(str(convergence_values))
+'''
 step = 1.0 / (dimensions - 1)
 
 for i in range(dimensions):
@@ -42,10 +42,9 @@ for i in range(dimensions):
 		convergent_points.append((0.5 * i, 1.0 - (0.5 * j)))
 
 print(convergent_points)
-
-data = open("data.txt", "w")
-
+'''
 def get_neighbours(s):
+	global square, dimensions
 	n = []
 	for i in range(square):
 		##print("Is " + str(i) + " a neighbour")
@@ -67,9 +66,9 @@ if(gui_available):
 		global u
 		window.fill((255, 255, 255))
 		pygame.draw.circle(window, (255, 0, 0), (int(screen_size * u[0][0]), int(screen_size * u[0][1])), 20)
-		pygame.draw.circle(window, (0, 255, 0), (int(screen_size * u[1][0]), int(screen_size * u[1][1])), 20)
-		pygame.draw.circle(window, (0, 0, 255), (int(screen_size * u[7][0]), int(screen_size * u[7][1])), 20)
-		pygame.draw.circle(window, (255, 255, 0), (int(screen_size * u[4][0]), int(screen_size * u[4][1])), 20)
+		pygame.draw.circle(window, (0, 255, 0), (int(screen_size * u[int((len(u)-1)/2)][0]), int(screen_size * u[int((len(u)-1)/2)][1])), 20)
+		pygame.draw.circle(window, (0, 0, 255), (int(screen_size * u[len(u) - 1][0]), int(screen_size * u[len(u) - 1][1])), 20)
+		#pygame.draw.circle(window, (255, 255, 0), (int(screen_size * u[4][0]), int(screen_size * u[4][1])), 20)
 		for i in range(square):
 			pos = (int(screen_size*u[i][0]), int(screen_size*u[i][1]))
 			for j in get_neighbours(i):
@@ -98,67 +97,96 @@ def update_learning_coefficient(a):
 	return 1 / (1 + log10(a)) 
 
 def calculate_convergence(i):
+	global u, convergent_points
 	pos = u[i]
 	conv_pos = convergent_points[i]
 	return sqrt(((pos[0] - conv_pos[0])**2) + ((pos[1] - conv_pos[1])**2))
 
-for i in range(iterations):
-	a = update_learning_coefficient(i + 1) # Uses i + 1 as log10 can't take 0 as input, has no affect on output
-	b = 0.5 * a
-	v1 = (random.random(), random.random())
-	v_points.append(v1)
-	s = get_min_distance(v1)
-	#print("\n\nPoint closest was " + str(s) + ", of value " + str(u[s]))
-	u[s][0] = u[s][0] + (a * (v1[0] - u[s][0]))
-	u[s][1] = u[s][1] + (a * (v1[1] - u[s][1]))
-	#print("Updated u" + str(s) + " is " + str(u[s]))
-	
-	neighbours = get_neighbours(s)
+experiments = [7] #[3, 5, 7]
+b_vals = [0.6, 0.7, 0.8, 0.9] #[x / 10 for x in range(10)]
+print(b_vals)
 
-	for n in neighbours:
-		u[n][0] = u[n][0] + (b * (v1[0] - u[n][0]))
-		u[n][1] = u[n][1] + (b * (v1[1] - u[n][1]))
-		#print("Neighbour " + str(n) + " updated to " + str(u[n]))
+for ex in experiments:
+	for b_val in b_vals:
+		print("Experimenting for " + str(ex) + "x" + str(ex) + " grid, b value " + str(b_val))
+		dimensions = ex
+		square = dimensions**2
+		random.seed(331) # use same seed every time 
 
-	for x in range(square):
-		temp_convergence[x] += calculate_convergence(x)
+		u = [[random.random(), random.random()] for x in range(square)]
+		v_points = []
+		convergent_points = []
+		temp_convergence = [0 for i in range(square)]
+		convergence_values = [[] for x in range(square)]
 
-	if(i % 100 == 0):
-		if(i != 0):
-			#print(str(i / 100))
-			for j, val in enumerate(convergence_values):
-				#print(val)
-				val.append(temp_convergence[j] / 100)
-				temp_convergence[j] = 0
+		step = 1.0 / (dimensions - 1)
+
+		for i in range(dimensions):
+			for j in range(dimensions):
+				convergent_points.append((step * i, 1.0 - (step * j)))
+
+		#print(convergent_points)
+
+		data = open("data_" + str(ex) + "_" + str(b_val) + ".txt", "w")
+
+		for i in range(iterations):
+			a = update_learning_coefficient(i + 1) # Uses i + 1 as log10 can't take 0 as input, has no affect on output
+			b = b_val * a
+			v1 = (random.random(), random.random())
+			v_points.append(v1)
+			s = get_min_distance(v1)
+			#print("\n\nPoint closest was " + str(s) + ", of value " + str(u[s]))
+			u[s][0] = u[s][0] + (a * (v1[0] - u[s][0]))
+			u[s][1] = u[s][1] + (a * (v1[1] - u[s][1]))
+			#print("Updated u" + str(s) + " is " + str(u[s]))
 			
-			average_convergence = 0
-			for v in convergence_values:
-				total = 0
-				for val in v:
-					total += val
-				total = total / len(v)
-				average_convergence += total
+			neighbours = get_neighbours(s)
 
-			average_convergence = average_convergence / square # Get average convergence	
-			data.write(str(average_convergence) + "\n")
-			'''
+			for n in neighbours:
+				u[n][0] = u[n][0] + (b * (v1[0] - u[n][0]))
+				u[n][1] = u[n][1] + (b * (v1[1] - u[n][1]))
+				#print("Neighbour " + str(n) + " updated to " + str(u[n]))
+
+			for x in range(square):
+				temp_convergence[x] += calculate_convergence(x)
+
+			if(i % 100 == 0):
+				if(i != 0):
+					#print(str(i / 100))
+					for j, val in enumerate(convergence_values):
+						#print(val)
+						val.append(temp_convergence[j] / 100)
+						temp_convergence[j] = 0
+					
+					average_convergence = 0
+					for v in convergence_values:
+						total = 0
+						for val in v:
+							total += val
+						total = total / len(v)
+						average_convergence += total
+
+					average_convergence = average_convergence / square # Get average convergence	
+					data.write(str(average_convergence) + "\n")
+					'''
+					total = 0
+					for v in convergence_values[4]:
+						total += v
+					total = total / len(convergence_values[4])
+
+					data.write(str(total) + "\n")
+					'''
+				if(gui_available):
+					show_points()
+
+		for i, val in enumerate(convergence_values):
 			total = 0
-			for v in convergence_values[4]:
-				total += v
-			total = total / len(convergence_values[4])
+			for j in val:
+				total += j
 
-			data.write(str(total) + "\n")
-			'''
-		if(gui_available):
-			show_points()
+			total = total / len(val)
+			#print("For point " + str(i) + ", convergence value " + str(total))
 
-for i, val in enumerate(convergence_values):
-	total = 0
-	for j in val:
-		total += j
+		data.close()
 
-	total = total / len(val)
-	print("For point " + str(i) + ", convergence value " + str(total))
-
-data.close()
 pygame.quit()
